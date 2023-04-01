856633b385cba9e69c13212299a0e3653e4be425
David Todorov <t_david@outlook.com>
2023-04-01 12:24:11 +0300

    initial

---
+++ src/components/GridComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.name"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </template>
+
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
