feb6b9c1352a89f825c36d20bdedf28f0b2aba91
David Todorov <t_david@outlook.com>
2023-04-06 12:34:36 +0300

    add loading component

--- src/components/GridComponent.vue
+++ src/components/GridComponent.vue
@@ -7,8 +7,16 @@
       </tr>
     </thead>
     <tbody>
-      <tr
-        v-for="item in peopleStore?.currentPeople"
+      <template v-if="isLoading">
+        <tr>
+          <td :colspan="headers.length">
+            <LoadingComponent></LoadingComponent>
+          </td>
+        </tr>
+      </template>
+      <template v-else>
+        <tr
+        v-for="item in currentPeople"
         :key="item.name"
       >
         <td>{{ item.name }}</td>
@@ -18,8 +26,10 @@
         <td>{{ formatDate(item.edited) }}</td>
         <td>{{ getStringValue(planetStore.planetsObj[item.homeworld]?.name) }}</td>
       </tr>
+      
+      </template>
       <tr>
-        <td colspan="6">
+        <td :colspan="headers.length">
           <v-pagination v-model="currentPage" :length="paginationLength"></v-pagination>
         </td>
       </tr>
@@ -29,12 +39,18 @@
 
 <script setup>
 import { ref, onMounted, watch } from 'vue'
+import { storeToRefs } from 'pinia'
 import { getStringValue, formatDate } from '../utils'
 import { usePlanetStore } from '../stores/planetsStore'
 import { usePeopleStore } from '../stores/peopleStore';
+import LoadingComponent from './LoadingComponent.vue'
 
 const planetStore = usePlanetStore();
 const peopleStore = usePeopleStore();
+
+const { currentPeople, pageAndPeople } = storeToRefs(peopleStore);
+const { getPeoplePerPage, setPageAndPeople } = peopleStore;
+
 let headers = [
   {
     displayName: 'Name'
@@ -55,24 +71,29 @@ let headers = [
     displayName: 'Planet Name'
   }
 ];
+const itemsPerPage = 10;
 
+let isLoading = ref(false);
 let paginationLength = ref(0);
 let currentPage = ref(1);
 
-const itemsPerPage = 10;
 
 onMounted( async () => {
-    await planetStore.getAllPlanets();
-    let result = await peopleStore.getPeoplePerPage();
-    getPaginationLength(result);
+  isLoading.value = true;
+  //await planetStore.getAllPlanets();
+  let result = await getPeoplePerPage();
+  getPaginationLength(result);
+  isLoading.value = false;
 });
 
 watch(currentPage, async (newPage)=> {
   //if data for the page was exists load it
-  if(peopleStore.pageAndPeople[newPage]) {
-    peopleStore.setPageAndPeople(newPage, peopleStore.pageAndPeople[newPage])
+  if(pageAndPeople[newPage]) {
+    setPageAndPeople(newPage, pageAndPeople[newPage])
   } else { //else get new data from server
-      await peopleStore.getPeoplePerPage(newPage);
+      isLoading.value = true;
+      await getPeoplePerPage(newPage);
+      isLoading.value = false;
   }
 });
 
@@ -80,9 +101,8 @@ function getPaginationLength(result) {
   paginationLength.value = Math.ceil(result.data.count / itemsPerPage);
 }
 
-
 </script>
 
-<style lang="">
+<style lang="css">
 
 </style>
---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="loadingContainer">
+        <v-progress-circular
+        indeterminate
+        color="blue"
+        ></v-progress-circular>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="css">
+  .loadingContainer {
+    display: flex;
+    justify-content: center;
+  }
+</style>
